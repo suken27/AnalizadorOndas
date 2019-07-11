0e6fb314788d0a115000a3584946da2d202b9ab4
suken27 <48246954+suken27@users.noreply.github.com>
2019-07-12 01:08:41 +0200

    View and file manager added.
    I created the main kivy view and separated the file reader code into a
    new class. Also the fileReader.py file now plots the data in a correct
    aspect ratio. Aspect ratio is something that must be fixed and controled
    by kivy in the view.

---
+++ src/FileManager.py
@@ -0,0 +1,39 @@
+# _*_ coding: utf-8 _*_
+
+import pandas as pd
+from datetime import datetime, timedelta
+
+class FileManager:
+    
+    filePath = ""
+    
+    def getDataFrame(self, filePath):
+        file = open(filePath, "r")
+        # This skips the first lane
+        file.readline()
+        # I keep the starting time of pressure monitoring to calculate the exact time of pressure recording
+        startingTime = datetime.strptime(file.readline().split("\t")[2][:-1], '%d/%m/%Y %H:%M:%S,%f')
+        # Skip the rest of the headers
+        for x in range(6):
+            file.readline()
+        
+        times = []
+        values = []
+        
+        for x in file:
+            lane = file.readline().split("\t")
+            exactTime = lane[0].split(",")
+            exactTimeSeconds = int(exactTime[0])
+            exactTimeMilliseconds = int(exactTime[1])
+            # Calculates the exact time when the pressure value was recorded
+            times.append(startingTime + timedelta(seconds=exactTimeSeconds, milliseconds=exactTimeMilliseconds))
+            values.append(int(lane[1]))
+        
+        file.close()
+        
+        dataFrame = pd.DataFrame({
+            'time': times,
+            'value': values
+        })
+        
+        return dataFrame
--- src/fileReader.py
+++ src/fileReader.py
@@ -1,8 +1,15 @@
+# _*_ coding: utf-8 _*_
+
 import pandas as pd
+from datetime import datetime, timedelta
+import numpy as np
 import matplotlib.pyplot as plt
 
 file = open("../input/KLH Hab 205 12Feb2019.txt", "r")
-for x in range(8):
+file.readline()
+startingTime = datetime.strptime(file.readline().split("\t")[2][:-1], '%d/%m/%Y %H:%M:%S,%f')
+print(startingTime)
+for x in range(6):
     file.readline()
 
 times = []
@@ -10,7 +17,10 @@ values = []
 
 for x in file:
     lane = file.readline().split("\t")
-    times.append(float(lane[0].replace(',','.')))
+    exactTime = lane[0].split(",")
+    exactTimeSeconds = int(exactTime[0])
+    exactTimeMilliseconds = int(exactTime[1])
+    times.append(startingTime + timedelta(seconds=exactTimeSeconds, milliseconds=exactTimeMilliseconds))
     values.append(int(lane[1]))
     
 dataFrame = pd.DataFrame({
@@ -18,7 +28,17 @@ dataFrame = pd.DataFrame({
     'value':values
 })
 
+
+plt.plot(dataFrame["time"], dataFrame["value"])
+plt.xlabel("Hora")
+plt.ylabel("Presión intracraneal")
 ax = plt.gca()
 
-dataFrame.plot(kind='line', x="time", y="value", ax=ax)
+ratio = 0.01
+xleft, xright = ax.get_xlim()
+ybottom, ytop = ax.get_ylim()
+# the abs method is used to make sure that all numbers are positive
+# because x and y axis of an axes maybe inversed.
+ax.set_aspect(abs((xright-xleft)/(ybottom-ytop))*ratio)
+
 plt.show()
